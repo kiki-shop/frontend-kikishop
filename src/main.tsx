bf5153e6f5bb19185bc862b3800ae9be6153900f
Phan Huy Hiệp <phanhuyhiep2k3@gmail.com>
2025-01-10 17:18:40 +0700

    add vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,13 @@ import App from './App'
 import { Provider } from 'react-redux'
 import { store } from './app/store/store'
 import { Toaster } from 'react-hot-toast'
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
+      <Analytics />
       <Toaster position='top-right' reverseOrder={false} />
     </Provider>
   </BrowserRouter>
